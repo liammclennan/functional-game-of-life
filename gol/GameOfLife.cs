9fa8b29972affe68fef25b825bfa088557fb6847
Liam McLennan <liam.mclennan@gmail.com>
2013-05-06 21:08:50 +1000

    data structure + rendering

---
+++ gol/GameOfLife.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace gol
+{
+    public class GameOfLife
+    {
+        public static string Render(List<Cell> cells)
+        {
+            var largestX = cells.Max(c => c.X);
+            var largestY = cells.Max(c => c.Y);
+            Func<int,int,bool, string> buildCell = (x,y,on) => 
+                string.Format("<div class=\"cell {2}\" style=\"left: {0}px; top: {1}px;\">&nbsp;</div>", 20 * (x+1), 20 * (y+1), @on ? "on" : "");
+
+            var results = from x in Enumerable.Range(0, largestX+1)
+                          from y in Enumerable.Range(0, largestY+1)
+                          select buildCell(x, y, IsOn(cells, x, y));
+            return string.Join("\n", results);
+        }
+
+        private static bool IsOn(IEnumerable<Cell> cells, int x, int y)
+        {
+            return cells.Any(c => c.X == x && c.Y == y);
+        }
+    }
+
+    public struct Cell
+    {
+        public int X;
+        public int Y;
+
+        public Cell(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
---
+++ gol/Tests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Givn;
+using NUnit.Framework;
+
+namespace gol
+{
+    [TestFixture]
+    public class RenderingTests
+    {
+        private List<Cell> _cells;
+        private string _result;
+
+        [Test]
+        public void RenderASmallSystem()
+        {
+            Giv.n(ASmallSystem);
+            Wh.n(ItIsRendered);
+            Th.n(() => ItShouldRenderDimensions(8, 17))
+                .And(() => LivingCellCountIs(_cells.Count));
+        }
+
+        private void ASmallSystem()
+        {
+            _cells = new List<Cell> { new Cell(1,0), new Cell(0,1), new Cell(2,1), new Cell(7,16) };
+        }
+
+        private void ItIsRendered()
+        {
+            _result = GameOfLife.Render(_cells);
+        }
+
+        private void ItShouldRenderDimensions(int x, int y)
+        {
+            Assert.AreEqual(x*y, CountOccurances(_result,"<div"));
+        }
+
+        private void LivingCellCountIs(int count)
+        {
+            Assert.AreEqual(count, CountOccurances(_result,"on"));
+        }
+
+        private static int CountOccurances(string source, string sub)
+        {
+            return source.Select((c, i) => source.Substring(i)).Count(s => s.StartsWith(sub));
+        }
+    }
+}
